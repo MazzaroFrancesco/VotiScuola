b91be4681c72d0184e35065afc8097d43b06e0f6
Francesco <127199376+MazzaroFrancesco@users.noreply.github.com>
2023-05-09 12:58:57 +0200

    Update Alunno.java

--- Alunno.java
+++ Alunno.java
@@ -2,9 +2,9 @@ public class Alunno extends Utente
 {
 private int idAlunno;
 
-public Alunno(int idAlunno){
-    setIdAlunno(idAlunno);
+public Alunno(int idAlunno,String nome,String cognome,String email,String password){
     super(nome,cognome,email,password);
+    setIdAlunno(idAlunno);
    
 }
 public void setIdAlunno(int idAlunno){
