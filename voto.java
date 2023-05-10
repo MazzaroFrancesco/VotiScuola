251e6e4d8ea203ec5e8dd35c47815fe041ea8760
Francesco <127199376+MazzaroFrancesco@users.noreply.github.com>
2023-05-10 09:35:28 +0200

    Update voto.java

---
+++ voto.java
@@ -0,0 +1,16 @@
+public class Voto {
+   private int Voto;
+private String Materia;
+   public Voto (int Voto){
+       setVoto (Voto);
+       
+   }
+   public void setVoto (int Voto){
+       this.Voto=Voto;
+       
+   }
+
+    public int getVoto(){
+    return Voto;
+}
+}
