a17ff206697fa413ed69385d12dbfea8e9ada75e
cennamoDJ7 <127200973+carminecennamo7@users.noreply.github.com>
2023-05-11 12:22:46 +0200

    Update voto.java

--- voto.java
+++ voto.java
@@ -1,16 +1,26 @@
+import java.time.LocalDate;
+
 public class Voto {
-   private int Voto;
+   LocalDate myObj;
+    
+   private float Voto;
 private String Materia;
-   public Voto (int Voto){
+  
+   public Voto (float Voto,int gg, int mm, int aa){
        setVoto (Voto);
+       setData(aa,mm,gg);
+       
        
    }
-   public void setVoto (int Voto){
+   public void setVoto (float Voto){
        this.Voto=Voto;
        
    }
-
-    public int getVoto(){
+public void setData (int gg, int mm, int aa){
+       myObj=LocalDate.of(aa, mm, gg);
+       
+   }
+    public float getVoto(){
     return Voto;
 }
 }
