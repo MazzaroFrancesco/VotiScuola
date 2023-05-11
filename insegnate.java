5807035175cf4da83447a67583629ef70b793d45
Pacconoe <127199890+Pacconoe@users.noreply.github.com>
2023-05-11 12:35:15 +0200

    Update insegnate.java

--- insegnate.java
+++ insegnate.java
@@ -1,7 +1,7 @@
 public final class Insegnante extends Utente {
      private int idInsegnante;
   
-    private String materia;
+    private Associazione Classe;
     
     public Insegnante (int idInsegnante, String materia,String nome,String cognome,String email,String password){
     super(nome,cognome,email,password);{
@@ -13,13 +13,20 @@ public final class Insegnante extends Utente {
     public  void setIdInsegnante(int idInsegnante) {
         this.idInsegnante=idInsegnante;
     }
+          public  Associazione Classe(Associazione Classe) {
+        this.Classe=Classe;
+          }
      public int getIdInsegnante() {
      
          return idInsegnante;
     }      
+         
  public  void setMateria(String materia) {
         this.materia=materia;
     }
+         public  Associazione Classe(Associazione Classe) {
+        return Classe;
+          }
    public String getMateria() {
      
          return materia;
